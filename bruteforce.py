903af33d12a373616e8ae092aa34382194844b8a
Sarvajnya Pujari <pujarsarvajnya5@gmail.com>
2025-02-28 23:24:19 +0530

    CF contest and debugging

--- bruteforce.py
+++ bruteforce.py
@@ -2,19 +2,9 @@ a = [4 ,5 ,8 ,8 ,8 ,4, 4 ,5 ,2, 3]
 b = [4, 5, 8, 2, 8, 4, 3, 5, 2, 3]
 n = len(a)
 from template import *
-# with open('debug.txt', 'w') as f:
-if 1:
-    seen=set()
-    for i in range(1, 100000+1):
-    # for j in range(1, 1001):
-        val1, val2 = sum(int(k) for k in str(i)), sum(int(k) for k in str(i+1))
-        # f.write(f"{val1} {val2} \n")
-        seen.add(val2-val1)
-        # if val2-val1 != 1:
-            # print(val1, val2, i, i+1, sep='\t')
-            
-            # f.write(f"{val1} {val2} \n")
-    print(seen)
+with open('debug.txt', 'w') as f:
+    for i in range(1, 500001):
+        f.write(f"{i}\n")
     
         
 # print(True)
