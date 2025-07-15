011acee79b5f4d61863429695f3db341bc92b098
Sarvajnya Pujari <pujarsarvajnya5@gmail.com>
2025-07-15 21:06:20 +0530

    Updated

--- bruteforce.py
+++ bruteforce.py
@@ -1,10 +1,24 @@
-a = [4 ,5 ,8 ,8 ,8 ,4, 4 ,5 ,2, 3]
-b = [4, 5, 8, 2, 8, 4, 3, 5, 2, 3]
-n = len(a)
-from template import *
+# a = [4 ,5 ,8 ,8 ,8 ,4, 4 ,5 ,2, 3]
+# b = [4, 5, 8, 2, 8, 4, 3, 5, 2, 3]
+# n = len(a)
+# from template import *
 with open('debug.txt', 'w') as f:
-    for i in range(1, 500001):
-        f.write(f"{i}\n")
+#     for i in range(1, 500001):
+#         f.write(f"{i}\n")
     
         
-# print(True)
+# # print(True)
+    
+    s=set()
+    for i in range(1, 10001):
+        o,e=0,0
+        
+        for j in range(1, i+1):
+            if i%j == 0:
+                if j&1:
+                    o+=1 
+                else:
+                    e+=1 
+        if e%o == 0:
+            f.write(f"{o}\n")
+            s.add(o)
